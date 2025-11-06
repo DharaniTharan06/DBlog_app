6f3cf77539fc998e7136b2082ef94eac02ef46a7
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-11-06 23:19:01 +0530

    Adding pages

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Addpost from './Addpost'
+import Allpost from './Allpost'
+import Editpost from './Editpost'
+import Home from './Home'
+import Login from './Login'
+import Post from './Post'
+import Signup from './Signup'
+export {Addpost , Allpost , Editpost , Home , Login , Post , Signup}
