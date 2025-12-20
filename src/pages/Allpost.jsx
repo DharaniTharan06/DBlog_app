3004c530972601bf3a463790d753da7f9c1e7bdb
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-12-20 20:44:14 +0530

    Ui changes

--- src/pages/Allpost.jsx
+++ src/pages/Allpost.jsx
@@ -12,11 +12,16 @@ function Allpost() {
     <div className=' w-full py-8'>
       <Container>
         <div className='flex flex-wrap'>
-            {posts.map((post) => (
+            {posts.length > 0 ? 
+              (posts.map((post) => (
                 <div key={post.$id} className='p-2 w-1/4'>
                     <PostCard post = {post}/>
                 </div>
-            ))}
+              ))) : (
+              <div className="w-full text-center">
+                  <h1 className="text-2xl font-bold">Go add some posts!</h1>
+              </div>
+            )}
         </div>
       </Container>
     </div>
